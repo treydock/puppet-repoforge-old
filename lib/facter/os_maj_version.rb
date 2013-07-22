9773c399202118186b5d7b3f6a7b901d2b8f1e9b
Trey Dockendorf <treydock@gmail.com>
2013-07-22 17:10:25 -0500

    - Removed check as pluginsync makes it pointless

--- lib/facter/os_maj_version.rb
+++ lib/facter/os_maj_version.rb
@@ -3,12 +3,9 @@
 # stuff I often don't require. This fact is quick to load so it shouldn't be
 # much of an issue.
 
-# Only add this new fact if not already defined by another module
-if ! Facter[:os_maj_version]
-  Facter.add(:os_maj_version) do
-    setcode do
-      v = Facter.value(:operatingsystemrelease)
-      v.split('.')[0].strip
-    end
+Facter.add(:os_maj_version) do
+  setcode do
+    v = Facter.value(:operatingsystemrelease)
+    v.split('.')[0].strip
   end
 end
